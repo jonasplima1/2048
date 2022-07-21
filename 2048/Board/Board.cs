4d3fd8ce64dae41f9323f29cd1bd91c46576d9a3
Jonas Pereira De Lima <jonasplima1@gmail.com>
2022-07-20 22:37:01 -0300

    Movimentação superior e inferior

---
+++ 2048/Board/Board.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2048.Board
+{
+    public class Board
+    {
+        public void StartGame()
+        {
+            Console.Clear();
+            int[,] board = new int[4, 4];
+            GenerateNewBlock(ref board);
+            GenerateNewBlock(ref board);
+            PrintBoard(board);
+        WAITUSER:
+            Console.Clear();
+            PrintBoard(board);
+            //Wait for player input
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.DownArrow:
+                    GoDown(ref board);
+                    GenerateNewBlock(ref board);
+                    goto WAITUSER;
+                case ConsoleKey.UpArrow:
+                    GoUp(ref board);
+                    GenerateNewBlock(ref board);
+                    goto WAITUSER;
+                case ConsoleKey.LeftArrow:
+                    GoLeft();
+                    goto WAITUSER;
+                case ConsoleKey.RightArrow:
+                    GoRight();
+                    goto WAITUSER;
+                case ConsoleKey.Home:
+                    return;
+                default:
+                    goto WAITUSER;
+            }
+        }
+        private void PrintBoard(int[,] matrix)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //Given a direction, combine the numbers on matrix
+        private void GoDown(ref int[,] board)
+        {
+            int[] stop = { 0, 0 };
+            for (int i = 0; i < 3; i++)
+            {
+                var temp = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        continue;
+                    }
+                    if (board[i + 1, j] == 0)
+                    {
+                        temp = board[i, j];
+                        board[i, j] = 0;
+                        board[i + 1, j] = temp;
+                    }
+                    if (board[i + 1, j] == board[i, j])
+                    {
+                        temp = board[i, j];
+                        board[i, j] = 0;
+                        board[i + 1, j] += temp;
+                    }
+                }
+            }
+        }
+        private void GoUp(ref int[,] board)
+        {
+            int[] stop = { 0, 0 };
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                var temp = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        continue;
+                    }
+                    if (board[i - 1, j] == 0)
+                    {
+                        temp = board[i, j];
+                        board[i, j] = 0;
+                        board[i - 1, j] = temp;
+                    }
+                    if (board[i - 1, j] == board[i, j])
+                    {
+                        temp = board[i, j];
+                        board[i, j] = 0;
+                        board[i - 1, j] += temp;
+                    }
+                }
+            }
+        }
+        private void GoLeft()
+        {
+
+        }
+        private void GoRight()
+        {
+
+        }
+
+
+        //Given a matrix, generate a new number in empty block | 90% chance to spawn 2 and 10% 4
+        private int[,] GenerateNewBlock(ref int[,] matrix)
+        {
+            //Generate list of empty positions
+            var emptyBlocks = GetEmptyBlocks(matrix);
+            if (emptyBlocks.Count == 0)
+            {
+                return matrix;
+            }
+            //Select empty coordinate
+            var coordinate = SelectPosition(emptyBlocks);
+            //Generate new number on that position
+            matrix[coordinate[0], coordinate[1]] = GetNumber();
+            //Return updated matrix;
+            return matrix;
+            //Generate number using the chances given
+            int GetNumber()
+            {
+                Random random = new Random();
+                const double margin = 90.0 / 100.0;
+                return random.NextDouble() <= margin ? 2 : 4;
+            }
+        }
+
+        //Generate a random coordinate for a 4x4 matrix
+        private int[] GetRandomPosition()
+        {
+            int min = 0;
+            int max = 4;
+            Random random = new Random();
+            int x = random.Next(min, max);
+            int y = random.Next(min, max);
+            return new int[] { x, y };
+        }
+
+        //Given a matrix, return list of empty coordinates
+        private List<int[]> GetEmptyBlocks(int[,] matrix)
+        {
+            List<int[]> blocks = new List<int[]>();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        int[] temp = { i, j };
+                        blocks.Add(temp);
+                    }
+                }
+            }
+            return blocks;
+        }
+
+        //Given a list of empty positions, pick one of the coordinates
+        private int[] SelectPosition(List<int[]> emptyBlocks)
+        {
+            Random random = new Random();
+            int x = random.Next(0, emptyBlocks.Count);
+            return emptyBlocks[x];
+        }
+    }
+
+
+}
---
+++ 2048/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using C2048.Board;
+
+START:
+Console.WriteLine("Welcome to 2048 C#! Press ENTER to start playing or HOME to get some help.");
+var output = Console.ReadKey().Key;
+Thread.Sleep(100);
+if (output == ConsoleKey.Enter)
+{
+    //Calls method to start game;
+    Board Board = new Board();
+    Board.StartGame();
+} 
+else if (output == ConsoleKey.Home)
+{
+    Thread.Sleep(100);
+    Console.WriteLine("-----------------------");
+    Console.WriteLine("HELP!");
+    Console.WriteLine("-----------------------");
+    Console.WriteLine("The objective here is to build a block with the number 2048 by moving the board around");
+    Console.WriteLine("You can do that by:");
+    Console.WriteLine("Press ↓, to push the board down");
+    Console.WriteLine("Press ↑, to push the board up");
+    Console.WriteLine("And... Well, you get it.");
+    Console.WriteLine("Or you can use the force. Remember that the numbers always sums up.");
+    Console.WriteLine("If you want to quit or restart the game, press HOME! Try it right now.");
+WAITING:
+    var outputHelp = Console.ReadKey().Key;
+    if (outputHelp == ConsoleKey.Home)
+    {
+        Console.Clear();
+        goto START;
+    }
+    else
+    {
+        Console.WriteLine("I said HOME!");
+        goto WAITING;
+    }
+}
+else
+{
+    Console.Clear();
+    goto START;
+}
+
+
+
+
+
